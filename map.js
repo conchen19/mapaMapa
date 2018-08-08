0b65110ac66dbe991e6a81ae085c6eb9816f2a2f
conchen19 <conniechen04@gmail.com>
2018-08-08 09:16:40 -0400

    Adding map and buttons

--- map.js
+++ map.js
@@ -27,3 +27,39 @@ document.addEventListener("DOMContentLoaded", function(e) {
     console.log("loaded")
     init();
 });
+
+function panHome() {
+    view.animate ({
+        center: ourLoc,
+        duration: 2000
+    });
+}
+
+function panToLocation() {
+    var countryName = document.getElementById("country-name").value;
+    query = query.replace(/ /g, "%20");
+    alert(query);
+
+    var countryRequest = new XMLHttpRequest(); 
+    countryRequest.open('GET', query, false); 
+
+    countryRequest.send(); 
+
+    var countryInformation = JSON.parse(countryRequest.response(Text))
+
+    if (countryName === "United States"){
+        alert("You did not enter a real country name!");
+        return; 
+    }
+    var query = "https://restcountries.eu/rest/v2/name/" +countryName; 
+
+    var lon = countryInformation[0].lonlng[1];
+    var lat = countryInformation[0].latlng[0];
+    var location = ol.proj.fromLonLat([lon,lat]);
+
+    view.animate({
+        center: location,
+        duration: 2000
+    });
+}
+
